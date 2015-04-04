85b76c2bd173f5c2328a71c889ffd8ed27a23d60
bgr-ubuntu-vm <bgr.gyk@gmail.com>
2015-04-04 23:48:31 +0200

    Always start from third chord

--- progressions.py
+++ progressions.py
@@ -23,23 +23,20 @@ progression_map = {
 }
 
 
-def random_progression():
-    prev = I
+def random_progression(next_val=III):
     while True:
-        next_val = random.choice(progression_map[prev])
         yield next_val
-        prev = next_val
+        next_val = random.choice(progression_map[next_val])
 
 
 progression = [n for n, _ in zip(random_progression(), range(32 + 1))]
-chords = progressions.to_chords(progression, "C")
+chords = progressions.to_chords(progression, "A")
 ncs = [NoteContainer([Note(note) for note in chord]) for chord in chords]
 
 track = Track()
 for nc in ncs:
     track.add_notes(nc, duration=4)
 
-#print(track)
 
 lily = lilypond.from_Track(track)
 print(lily)
