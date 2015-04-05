ffd6fffdd413ccd2d9c50c13d6046baca55fdc2d
bgr-ubuntu-vm <bgr.gyk@gmail.com>
2015-04-05 03:29:53 +0200

    Use seeded random, complete lilypond output

--- progressions.py
+++ progressions.py
@@ -3,6 +3,12 @@ from mingus.core import progressions
 from mingus.containers import NoteContainer, Note, Track
 from mingus.extra import lilypond
 
+
+TITLE = "Randomations No. 16"
+KEY = "A"
+LENGTH = 64
+
+
 I   = "I"
 II  = "II"
 III = "III"
@@ -24,13 +30,14 @@ progression_map = {
 
 
 def random_progression(next_val=III):
+    rand = random.Random(TITLE)
     while True:
         yield next_val
-        next_val = random.choice(progression_map[next_val])
+        next_val = rand.choice(progression_map[next_val])
 
 
-progression = [n for n, _ in zip(random_progression(), range(32 + 1))]
-chords = progressions.to_chords(progression, "A")
+progression = [n for n, _ in zip(random_progression(), range(LENGTH + 1))]
+chords = progressions.to_chords(progression, KEY)
 ncs = [NoteContainer([Note(note) for note in chord]) for chord in chords]
 
 track = Track()
@@ -38,5 +45,30 @@ for nc in ncs:
     track.add_notes(nc, duration=4)
 
 
-lily = lilypond.from_Track(track)
-print(lily)
+lily_template = """
+\\version "2.18.0"
+
+\\header {{
+    title = "{title}"
+    composer = "bgr's Python script"
+}}
+
+prog = {notes}
+
+\\score {{
+
+  \\new Staff {{
+    \\time 4/4
+
+    \\prog
+  }}
+
+  \\layout {{}}
+  \\midi {{}}
+}}
+""".strip()
+
+print(lily_template.format(
+    title=TITLE,
+    notes=lilypond.from_Track(track)
+))
